b09a9f7e4c7f87659611678e91746e4f976dae5a
Karan Kumar <152979583+CodeByKaran@users.noreply.github.com>
2024-04-18 14:16:56 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://note-app-server-six.vercel.app'
+      '/api':'https://note-app-server-six.vercel.app/'
     }
   },
   plugins: [react()],
