b2752a80f69e171e892d2138c91b2049f1eec98c
Karan Kumar <152979583+CodeByKaran@users.noreply.github.com>
2024-04-20 01:01:25 +0530

    Update SignUp.jsx

--- Frontend/src/components/SignUpPage/SignUp.jsx
+++ Frontend/src/components/SignUpPage/SignUp.jsx
@@ -28,7 +28,7 @@ export default function SignUp() {
     
     try {
       console.log("run hua")
-      let {data} = await axios.post("/api/v1/users/signup",userCredentials)
+      let {data} = await axios.post("https://noteapp-aznr.onrender.com/api/v1/users/signup",userCredentials)
       setLogging(false)
       console.log(data)
       if(data.statusCode===200){
