9915bd2ccd09fcd888fa5c62d1ad5d7cf2b8d859
Karan Kumar <152979583+CodeByKaran@users.noreply.github.com>
2024-04-20 01:02:36 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  /*server:{
     proxy:{
       '/api':'https://noteapp-aznr.onrender.com'
     }
-  },
+  },*/
   plugins: [react()],
 })
