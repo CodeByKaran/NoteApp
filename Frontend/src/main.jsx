e7a569bd62f472136d7fee6e10ae57293a7b810a
CodeByKaran <karankumar010010@gmail.com>
2024-04-20 17:42:57 +0530

    update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,11 +9,11 @@ import {
 } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  
 )
