44b5bae7819a63c97dc3db775fd59d07f41a8c12
Karan Kumar <152979583+CodeByKaran@users.noreply.github.com>
2024-04-21 15:18:26 +0530

    Update Note.jsx

--- Frontend/src/components/Note/Note.jsx
+++ Frontend/src/components/Note/Note.jsx
@@ -44,7 +44,7 @@ export default function Note({title,content,id,refreshNote}) {
    }
 
   return (
-        <div className='h-[220px] bg-gray-300 w-[45%] rounded-lg shadow-xl relative hover:bg-gray-400 transition-all duration-500 hover:text-gray-100 my-4 pb-3' onMouseLeave={()=>setisMenuClicked(false)}
+        <div className='h-fit bg-gray-300 w-[45%] rounded-lg shadow-xl relative hover:bg-gray-400 transition-all duration-500 hover:text-gray-100 my-4 pb-3' onMouseLeave={()=>setisMenuClicked(false)}
          >
             <h3 className='absolute left-3 top-2 text-sm font-medium cursor-default' >{title}</h3>
 
@@ -58,7 +58,7 @@ export default function Note({title,content,id,refreshNote}) {
             
             </div>
 
-            <p className='text-[14px] mt-[45px]  text-start p-3 cursor-default break-words myNote' onClick={GotoEdit}> 
+            <p className='text-[14px] mt-[45px]  text-start p-2 cursor-default break-words' onClick={GotoEdit}> 
                 {content.length>140?content.toString().slice(0,140):content}
             </p>
         </div>
